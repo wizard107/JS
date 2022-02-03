315d445a04904753b87cc226577f13cfc2adf181
athar <athar.adel@gmx.de>
2022-02-03 12:30:42 +0100

    stopwatch

--- site.js
+++ site.js
@@ -2,6 +2,44 @@ function sayHello(){
 	alert("Hello");
 }
 
+
+
 function changeBackground(color){
     document.body.style.background = color;
+}
+var counter = 0;
+var intervalID = 0;
+function myTimer(){
+    let min = floor(counter/60);
+    let sec = counter%60; 
+    str = min.toString();
+    str2 = sec.toString();
+    str3 = str+':'+str2;
+    counter++;
+    document.getElementById("demo").innerHTML = str3;
+    document.getElementById("demo").innerHTML = "hello";
+}
+var countdown;
+function myTimer2() {
+    countdown = setInterval(myTimer3,1000);
+    myTimer3();
+  }
+
+function myTimer3(){
+    var min = Math.floor(counter/60);
+    var sec = counter%60;
+    str1 = min.toString();
+    var c = counter++;
+    
+    document.getElementById("demo").innerHTML = min.toString() + ':' + sec.toString();
+}
+
+function stopInterval(){
+    clearInterval(countdown);
+}
+
+function Reset(){
+    stopInterval(countdown);
+    counter = 0;
+    document.getElementById("demo").innerHTML = "0:0";
 }
